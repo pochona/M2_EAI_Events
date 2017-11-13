d66183415aae9ce1d14f9fc73f58cc5b1922832b
Amaury Pochon <amaury.pochon@univ-tlse3.fr>
2017-11-13 14:39:43 +0100

    Nommage (topic, queue, factory)

---
+++ ProjetEvents/ProjetEvents-ejb/src/java/messages/Nommage.java
@@ -0,0 +1,25 @@
+package messages;
+
+/**
+ *
+ * @author elavinal
+ */
+public class Nommage {
+    
+    public final static String FABRIQUE_CONNEXIONS = "jms/ConnectionFactoryEvents";
+    
+    public final static String TOPIC_DEMANDE = "jms/Event_Demande";
+    public final static String TOPIC_REPONSE = "jms/Event_Reponse";
+    public final static String TOPIC_PROJET = "jms/Event_Projet";
+    public final static String TOPIC_CONFIRMATION = "jms/Event_Confirmation";
+    
+    public final static String QUEUE_SALLE = "jms/Event_Salle";
+    public final static String QUEUE_ESTIMATION = "jms/Event_Estimation";
+    /*
+    public final static String MSG_STOCK = "STOCK";
+    public final static String MSG_FACTURATION = "FACTURATION";
+    public final static String MSG_CMD_ANNULEE = "CMD_ANNULEE";
+    public final static String MSG_CMD_VALIDE = "CMD_VALIDE";
+    */
+    
+}
