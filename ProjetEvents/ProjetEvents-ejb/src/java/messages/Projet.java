46ddcf6d682634d4bea4e16074ece8231dd14bc1
Amaury Pochon <amaury.pochon@univ-tlse3.fr>
2017-11-17 14:19:43 +0100

    Création message

---
+++ ProjetEvents/ProjetEvents-ejb/src/java/messages/Projet.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package messages;
+
+/**
+ *
+ * @author Amaury_PC
+ */
+public class Projet {
+    private String nom;
+    private String coordonnees;
+    private String nomProjet;
+    private int nbParticipant;
+    private String date;
+    
+    private Salle salle;
+}
