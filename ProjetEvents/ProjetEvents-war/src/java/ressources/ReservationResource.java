2dbc272cf611d403df52643f5890fcfe88068138
DESKTOP-C0PDL2F\Basti <bastien.gaillard@live.fr>
2017-11-22 20:13:13 +0100

    Commit Babassssssss

--- ProjetEvents/ProjetEvents-war/src/java/ressources/ReservationResource.java
+++ ProjetEvents/ProjetEvents-war/src/java/ressources/ReservationResource.java
@@ -5,61 +5,53 @@
  */
 package ressources;
 
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import messages.Projet;
-import singleton.ProjetSingleton;
 
 /**
  * REST Web Service
  *
  * @author Basti
  */
-@Path("ws")
+@Path("reservation")
 public class ReservationResource {
-    
-    
-    ProjetSingleton projetSingleton = lookupProjetSingletonBean();
-    
+
     @Context
     private UriInfo context;
+
+    /**
+     * Creates a new instance of ReservationResource
+     */
+    public ReservationResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of ressources.ReservationResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
 
     /**
      * POST method for updating or creating an instance of CommandeResource
      * @param content representation for the resource
      */
     @POST
-    @Path("reservation")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String reservation(@FormParam("nom") String nom) {
-        return nom;
-    }
-    
-    
-     
-    private ProjetSingleton lookupProjetSingletonBean() {
-        try {
-            javax.naming.Context c = new InitialContext();
-            return (ProjetSingleton) c.lookup("java:global/ProjetEvents/ProjetEvents-ejb/ProjetSingleton!webserver.ProjetSingleton");
-        } catch (NamingException ne) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
-            throw new RuntimeException(ne);
-        }
+    public void postJson(@FormParam("nom") String nom) {
+        System.out.println(nom);
     }
 }
